238e627b457ee0b43f4fdb573138cd28b4436877
Nandpal Jethanand <nandpal@A-Nandpal-New.local>
2014-11-18 10:37:14 +0530

    Initial Commit

---
+++ Caption/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Caption
+//
+//  Created by Nandpal Jethanand on 11/18/14.
+//  Copyright (c) 2014 SoftwebSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Caption/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Caption
+//
+//  Created by Nandpal Jethanand on 11/18/14.
+//  Copyright (c) 2014 SoftwebSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
